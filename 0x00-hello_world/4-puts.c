2eb5c07a9d0e633d4ca0ffd2a61d3485f7c188fc
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-16 14:27:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
+/*
+ * File: 4-puts.c
+ */
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+	puts("'Programming is like building a multilingual puzzle'\n");
 
 		return (0);
 }
