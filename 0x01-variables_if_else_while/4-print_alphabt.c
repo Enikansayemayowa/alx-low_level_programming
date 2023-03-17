1a839f0893b7cfb4c66fc29bf56b5b03aefc7302
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-17 15:48:10 -0700

    lowwer case with condition

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -17,11 +17,10 @@ int main(void)
 {
 	char letter;
 	int i;
-       
 	letter = 'a';
 	for (i = 0; i < 26; i++)
-		if (!(letter + i == 'q'|| letter + i == 'e'))
-			putchar(letter + i);
-       	putchar('\n');
-        return (0);
+	       if (!(letter + i == 'q'|| letter + i == 'e'))
+		       putchar(letter + i);
+	putchar('\n');
+	return (0);
 }
