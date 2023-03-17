acd84e47055d5accb911c28fe78b1d55ebed2630
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-17 15:40:11 -0700

    lowwer case with condition

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+/*
+ * file: 4-print_alphabt.c
+ * Description: This program prints
+ * all the lower case characters in
+ * the alphabet, then the uppercase
+ * using putchar()
+ */
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - this is the main function to run everything
+ * @void: no argument pass in
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	int i;
+       
+	letter = 'a';
+	for (i = 0; i < 26; i++)
+		if (!(letter + i == 'q'|| letter + i == 'e'))
+			putchar(letter + i);
+       	putchar('\n');
+        return (0);
+}
