c433380152d32107f9d9bbf65f580d36ee8045a6
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-17 16:59:27 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+/*
+ * 8-print_tebahpla.c
+ *
+ * Description:
+ * This program prints
+ * all the single digit hexadecimal
+ * numbers
+ *
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter, num;
+	int i;
+
+	letter = 'a';
+	number = '0';
+
+	for (i = 0; i < 16; i++)
+		putchar(num + i);
+	putchar('\n');
+	return (0);
+}
