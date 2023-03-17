a8dec7b10d434bb6d3b723e8608121983b3a4485
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-17 16:38:31 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+/*
+ * 6-print_numberz.c
+ *
+ * Description:
+ * This program prints
+ * all the base 10 digits in order
+ * using putchar()
+ * No vars of type char ...
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 28; i < 38; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
