971da294548fe6bccb08a47d997a4690a3439cae
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-21 16:18:40 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+/*
+ * file: 5-sign.c
+ */
+#include "main.h"
+/**
+ * Sign - Write a function that prints the sign of a number.
+ *
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
