a77c3229b4c03915e461b7cbf9d636200c133daa
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-21 18:33:31 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,4 +7,33 @@
  *
  * returns - void
  */
+void times_table(void)
+{
+	int row, col, p;
 
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+		_putchar(',');
+		for (col = 0; col <= 9; col++)
+		{
+			p = col * row;
+			if (p / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + p);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + p / 10);
+				_putchar('0' + p % 10);
+			}
+			if (col < 9)
+				_putchar(',');
+			else
+				_putchar('\n');
+		}
+	}
+}
