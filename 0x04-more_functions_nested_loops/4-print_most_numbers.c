ceb349988b16d58bfd3584e5dcaab74bd4d22eeb
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-23 18:03:53 -0700

    error check 5 and 6

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
+
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9
- * excluding 2 and 4
+ * print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
  *
  * Return: void
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
 	i = 0;
-	while (i <= 9)
+	while (i >= '0' && i <= '9')
 	{
 		if (!(i == '2') || (i == '4'))
 		{
