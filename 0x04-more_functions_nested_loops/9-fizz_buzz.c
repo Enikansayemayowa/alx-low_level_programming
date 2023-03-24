53da5cd065d1341e39e66ec342d86c1179c0e90c
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-03-23 20:50:57 -0700

    fizzbuzz algorithm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - this is an implementation of the FizzBuzz(tm) algorithm
+ * Return: 0 upon successful exit
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+				if ((i % 3 == 0) && (i % 5 == 0))
+				{
+					printf("FizzBuzz");
+				}
+				else
+				{
+					printf(" %d", i);
+				}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
+}
