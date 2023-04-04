a7b98d3d4fab83e6a90286660fbc37b3989c397a
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-04 07:58:49 -0700

    segmented error fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,23 +10,26 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	if (*needle[0] == '\0')
+	if (needle[0] == '\0')
 	{
 		return (haystack);
 	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		*p2 = *p1;
-		*p3 = *needle;
-		while (*p2 != '\0' && *p3 != '\0' && *p2 == *p3)
+		if (haystack[i] == needle[0])
 		{
-			p2++;
-			p3++;
-		}
-		if (*p3 != '\0')
-		{
-			return (p1++);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
