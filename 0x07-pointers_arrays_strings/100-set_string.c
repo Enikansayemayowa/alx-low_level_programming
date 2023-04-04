55090ca630a5965eaa24d88ad21fec48fdaca12f
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-04 02:56:36 -0700

    betty value description resolved

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include"main.h"
 /**
  * set_string - function that sets the value of a pointer to a char
- * @s:
- * @to:
+ * @s: pointer to set value
+ * @to: value to point to
  * Return: void
  */
 void set_string(char **s, char *to)
