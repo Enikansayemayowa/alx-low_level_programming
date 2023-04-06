54dbb0e7bc27ed01ab92fe231e2bc6a811fd560c
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-05 21:21:31 -0700

    square root of natural number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to find the natural square root
+ * @l: lower number
+ * @h: higher number
+ * @mid: division of l and h by 2
+ * @mid_squared: square od mid
+ * @square_check: function for checking
+ * Return: 1 on success
+ */
+int square_check(int n, int l, int h)
+{
+	int mid = (l + h) / 2;
+	int mid_squared = mid * mid;
+
+	if (l > h)
+	{
+		return (-1);
+	}
+	if (mid_squared == n)
+	{
+		return (mid);
+	}
+	else
+		if (mid_squared < n)
+		{
+			return (square_check(n, mid + 1, h));
+		}
+		else
+		{
+			return (square_check(n, l, mid - 1));
+		}
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		if (n == 0 || n == 1)
+		{
+			return (n);
+		}
+		else
+		{
+			return (square_check(n, 1, n / 2));
+		}
+}
