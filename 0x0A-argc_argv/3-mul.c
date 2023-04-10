46c7e5f4d65e6d0c3215f1fae6623fe47a589ce3
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-10 06:28:28 -0700

    check for segmented error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,14 +6,16 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int result;
 	int value1 = atoi(argv[1]);
 	int value2 = atoi(argv[2]);
-	if (argc == 1)
+
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
+		return (1);
 	}
 	result = value1 * value2;
 	printf("%d\n", result);
