6bdd60404ab0004633a099eef08761fae7ab64df
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-12 16:38:00 -0700

    fixed segmented erroe

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,22 +20,30 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	newArray = malloc(sizeof(char) * (i + j + 1));
 	while (s1[i] != '\0')
 	{
 		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	newArray = malloc(sizeof(char) * (i + j + 1));
+	if (newArray == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
 		newArray[i] = s1[i];
 		i++;
 	}
 	while (s2[j] != '\0')
 	{
-		newArray[j] = s2[j];
+		newArray[i] = s2[j];
 		i++, j++;
-	}
-	/*newArray = malloc(sizeof(char) *(i + j + 1));*/
-	if (newArray == NULL)
-	{
-		return (NULL);
 	}
 	newArray[i] = '\0';
 	return (newArray);
