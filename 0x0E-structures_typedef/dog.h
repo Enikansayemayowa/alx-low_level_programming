213f7f89603dd21d07e3c976ae50937ff9190172
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-18 13:23:45 -0700

    check for bugs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,12 +20,7 @@ void print_dog(struct dog *d);
 /**
  * dog_t - typedef for dog_t
  */
-/*typedef struct dog dog_t;*/
-typedef struct {
-    char *name;
-    float age;
-    char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
