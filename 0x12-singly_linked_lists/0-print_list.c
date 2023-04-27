4c7f5c2abd90c5797f0a2826e580d01f4866a28c
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-04-27 10:49:39 -0700

    fixed betty error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer
+ * Return: size_t
+ */
 size_t print_list(const list_t *h)
 {
 	char count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
