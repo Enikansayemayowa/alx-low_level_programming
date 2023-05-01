80be770a100060f32f55a6ff2fa20d5bf62a57c6
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-05-01 00:06:57 -0700

    free pointer of pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_listint2 - Write a function that frees a listint_t list.
+ * @head: pointer to pointer
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+}
